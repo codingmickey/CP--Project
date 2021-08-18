efd0601c7bb169f2868a3d78f1fb86c32424d3ad
Kartik <kjmickey003@gmail.com>
2021-08-18 14:19:19 +0530

    Converted Code from C++ to C

--- Library.cpp
+++ Library.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
-using namespace std;
-
+#include <stdio.h>
+#include <string.h>
 struct Book
 {
-    string nameOfBook;
+    char nameOfBook[50];
     int numberOfCopies = 2;
-    string description;
+    char description[200];
     int bookTaken = 0;
 };
 
@@ -13,8 +12,8 @@ int main()
 {
     int fine = 0;
     struct Book Library[5];
-    string bookNames[5] = {"A TIME TO KILL", "EAST OF EDEN", "VILE BODIES", "MOAB IS MY WASHPOT", "NOLI ME TANGERE"};
-    string bookDescription[5] = {
+    char bookNames[5][50] = {"A TIME TO KILL", "EAST OF EDEN", "VILE BODIES", "MOAB IS MY WASHPOT", "NOLI ME TANGERE"};
+    char bookDescription[5][200] = {
         "This one is from 3:3 in the Ecclesiastes, again part of the Old Testament.",
         "Steinbeck apparently considered this 1952 novel to be his magnum opu",
         "Waugh took the title for his 1930 novel from Philippians, full name",
@@ -22,44 +21,45 @@ int main()
         "Rizal, one of the national heroes of the Philippines, wrote this"};
     int taskNumber, bookTaken = 0;
     char onceAgain;
+    char dummy;
 
     for (int i = 0; i < 5; i++)
     {
-        Library[i].nameOfBook = bookNames[i];
-        Library[i].description = bookDescription[i];
+        strcpy(Library[i].nameOfBook, bookNames[i]);
+        strcpy(Library[i].description, bookDescription[i]);
     }
     do
     {
-        cout << "\nWelcome to the library\n\n";
-        cout << "Books Present in the Library: \n";
+        printf("\nWelcome to the library\n\n");
+        printf("Books Present in the Library: \n");
         for (int i = 0; i < 5; i++)
         {
-            cout << i + 1 << ". " << Library[i].nameOfBook << "\n";
-            cout << "Description: " << Library[i].description << "\n";
+            printf("%d. %s\n", i + 1, Library[i].nameOfBook);
+            printf("Description: %s\n", Library[i].description);
         }
-        cout << "\nWhat do you want to do?\n";
-        cout << "1. Issue a book\n2. Return a book\n3. Show fine\nEnter the number(1/2/3): ";
-        cin >> taskNumber;
+        printf("\nWhat do you want to do?\n");
+        printf("1. Issue a book\n2. Return a book\n3. Show fine\nEnter the number(1/2/3): ");
+        scanf("%d", &taskNumber);
         if (taskNumber == 1)
         {
             int temp;
-            cout << "\nWhich book do you want to issue?\n";
+            printf("\nWhich book do you want to issue?\n");
             for (int i = 0; i < 5; i++)
             {
-                cout << i + 1 << ". " << Library[i].nameOfBook << "\n";
-                cout << "Count: " << Library[i].numberOfCopies << "\n";
+                printf("%d. %s\n", i + 1, Library[i].nameOfBook);
+                printf("Count: %d\n", Library[i].numberOfCopies);
             }
-            cout << "\nEnter the book number: ";
-            cin >> temp;
+            printf("\nEnter the book number: ");
+            scanf("%d", &temp);
             temp--;
             if (Library[temp].numberOfCopies == 0)
             {
-                cout << "Cannot issue this book as there are no copies avaliable!\n\n";
+                printf("Cannot issue this book as there are no copies avaliable!\n\n");
             }
             else
             {
                 (Library[temp].numberOfCopies)--;
-                cout << "\nCopy Issued!\nNow only " << Library[temp].numberOfCopies << " number of copies avaliable of that book.\n\n";
+                printf("\nCopy Issued!\nNow only %d number of copies avaliable of that book.\n\n", Library[temp].numberOfCopies);
                 Library[temp].bookTaken++;
                 bookTaken++;
             }
@@ -67,46 +67,47 @@ int main()
         else if (taskNumber == 2 && bookTaken > 0)
         {
             int bookNumber, numberOfDays;
-            cout << "\nWhich book do you want to return?\n";
+            printf("\nWhich book do you want to return?\n");
             for (int i = 0; i < 5; i++)
             {
-                cout << i + 1 << ". " << Library[i].nameOfBook << "\n";
+                printf("%d. %s\n", i + 1, Library[i].nameOfBook);
             }
-            cout << "\nEnter the book number: ";
-            cin >> bookNumber;
+            printf("\nEnter the book number: ");
+            scanf("%d", bookNumber);
             bookNumber--;
             if (Library[bookNumber].bookTaken > 0)
             {
-                cout << "How many days ago the book was issued?\n-";
-                cin >> numberOfDays;
+                printf("How many days ago the book was issued?\n-");
+                scanf("%d", numberOfDays);
                 if (numberOfDays > 7)
                 {
                     fine += (numberOfDays - 7) * 10;
                 }
                 (Library[bookNumber].numberOfCopies)++;
-                cout << "\nBook Returned!\nNow " << Library[bookNumber].numberOfCopies << " copies avaliable of that book.\n\n";
+                printf("\nBook Returned!\nNow %d copies avaliable of that book.\n\n", Library[bookNumber].numberOfCopies);
                 bookTaken--;
                 Library[bookNumber].bookTaken--;
             }
             else
             {
-                cout << "\nFirst Issue that book then return lol.\n\n";
+                printf("\nFirst Issue that book then return lol.\n\n");
             }
         }
         else if (taskNumber == 2 && !bookTaken)
         {
-            cout << "\nFirst Issue a book!\n\n";
+            printf("\nFirst Issue a book!\n\n");
         }
         else if (taskNumber == 3)
         {
-            cout << "\nYou have a fine of Rs." << fine << "\n\n";
+            printf("\nYou have a fine of Rs.%d\n\n", fine);
         }
         else
         {
-            cout << "Enter a valid option!!\n";
+            printf("Enter a valid option!!\n");
         }
-        cout << "Do you want to continue?(Y/N)\n-";
-        cin >> onceAgain;
+        printf("Do you want to continue?(Y/N)\n-");
+        scanf("%c", &dummy);
+        scanf("%c", &onceAgain);
 
     } while (onceAgain == 'y' || onceAgain == 'Y');
 
